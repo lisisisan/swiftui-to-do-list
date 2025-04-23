e47d692442e0aeeab878ff88863fbd6f393340e4
lisisian <125748777+lisisisan@users.noreply.github.com.>
2025-04-23 22:01:58 +0500

    initial commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Ilona on 4/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
